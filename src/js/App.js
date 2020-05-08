5523d36dcdd9c5e5e639ee84f2b6a7b3c8abb3bc
bluninja1234 <31595285+bluninja1234@users.noreply.github.com>
2020-05-08 14:38:01 -0700

    update

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,42 @@
 import React from 'react';
-import './App.css';
+import '../css/App.css';
 
+export default class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      taskArray: []
+    }
+  }
+
+  render() {
+    var taskArray = [];
+    for (var i = 0; i < 10; i++)
+    {
+      taskArray.push(<Task value={"test" + i} key={i} />)
+    }
+    return (
+      <div>
+        {taskArray}
+      </div>
+    )
+  }
+}
+
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.value}
+      </div>
+    )
+  }
+}
